35e96077edebbcd1d7904d91080708c07eddf6e3
samiransari90 <68936255+samiransari90@users.noreply.github.com>
2020-07-29 09:17:37 +0530

    Add files via upload

---
+++ common.js
@@ -0,0 +1,11 @@
+function get(Control){
+    return document.getElementById(Control);
+}
+function getValue(control)
+{
+    return document.getElementById(control).value;
+}
+function setValue(control,value)
+{
+    return document.getElementById(control).value = value;
+}
